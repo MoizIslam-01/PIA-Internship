63403c1598d7144a2fbfb6b4d22a1688ff95c3b0
MoizIslam-01 <smonarch421@gmail.com>
2025-09-10 13:55:27 +0500

    Navbar Sidebar BarChart

--- Bootstrap/script.js
+++ Bootstrap/script.js
@@ -1,12 +1,12 @@
 const ctx = document.getElementById("myBarChart").getContext("2d");
 const myBarChart = new Chart(ctx, {
-  type: "pie",
+  type: "bar",
   data: {
     labels: ["Admins", "Editors", "Subscribers"],
     datasets: [
       {
         label: "Users",
-        data: [15, 8, 12], // Example data
+        data: [105, 198, 152],
         backgroundColor: ["#0d6efd", "#198754", "#ffc107"], // Bootstrap colors
         borderRadius: 5,
       },
