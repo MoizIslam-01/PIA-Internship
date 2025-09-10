aef7b8cb8e1ef2ab9d97258378d9332acc33c44d
MoizIslam-01 <smonarch421@gmail.com>
2025-09-10 13:14:03 +0500

    bootstrap

---
+++ Bootstrap/script.js
@@ -0,0 +1,27 @@
+const ctx = document.getElementById("myBarChart").getContext("2d");
+const myBarChart = new Chart(ctx, {
+  type: "pie",
+  data: {
+    labels: ["Admins", "Editors", "Subscribers"],
+    datasets: [
+      {
+        label: "Users",
+        data: [15, 8, 12], // Example data
+        backgroundColor: ["#0d6efd", "#198754", "#ffc107"], // Bootstrap colors
+        borderRadius: 5,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    plugins: {
+      legend: { display: false },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: { stepSize: 2 },
+      },
+    },
+  },
+});
